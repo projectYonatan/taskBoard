d9f736c8829ebba225b3be288a211c0eff0ac710
projectYonatan <dixie.fever@gmail.com>
2022-03-23 23:44:30 +0200

    add notes display functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ const detailsBox = document.getElementById("detailsBox");
 const dateBox = document.getElementById("dateBox");
 const timeBox = document.getElementById("timeBox");
 
+function initializePage() {
+    clearForm();
+    logNotes();
+    displayNotes();
+}
+
 //========Form==========
 
 function isFormValid() {
@@ -40,11 +46,44 @@ function saveNotes(task) {
     localStorage.setItem("notes", jsonNotesArray);
 }
 
-function printNotes() {
+function logNotes() {
     const notes = loadNotes();
     console.log(notes);
 }
 
+function displayNotes() {
+    const notes = loadNotes();
+    const notesAllWrapper = document.getElementById("notes-all-wrapper");
+    notesAllWrapper.innerHTML = "";
+
+    for (let note of notes) {
+        const noteWrapper = createNoteElement(note);
+        notesAllWrapper.append(noteWrapper);
+    }
+}
+
+function createNoteElement(note) {
+    const noteWrapper = document.createElement("div");
+    const noteContainer = document.createElement("div");
+    const noteDetails = document.createElement("div");
+    const noteDate = document.createElement("div");
+    const noteTime = document.createElement("div");
+
+    noteWrapper.id = "note-wrapper";
+    noteContainer.id = "note-container";
+    noteDetails.id = "note-details";
+    noteDate.id = "note-date";
+    noteTime.id = "note-time";
+
+    noteDetails.innerHTML = note.details;
+    noteDate.innerHTML = note.date;
+    noteTime.innerHTML = note.time;
+
+    noteContainer.append(noteDetails, noteDate, noteTime);
+    noteWrapper.append(noteContainer);
+
+    return noteWrapper;
+}
 
 
 //========Task==========
@@ -66,5 +105,6 @@ function saveTask() {
     const task = createTask();
     saveNotes(task);
     clearForm();
-    printNotes();
+    logNotes();
+    displayNotes();
 }
